8aae0b89dbb5839324d014cfed5a79f6bdf7b73d
mansour0907 <mohamed.mansour1@usmba.ac.ma>
2023-11-29 19:29:10 +0100

    deleted:    public/favicon.ico 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	new file:   public/makeover4.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js deleted:    src/setupTests.js 	new file:   src/styles.css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+function App() {
+  return (
+    <div className="card">
+      <Avatar name="avatar" avatar="makeover4.png" />
+      <div className="data">
+        <Intro
+          IntroName="Mohamed MANSOUR"
+          description="I am Mohamed Mansour, currently undergoing training as a full-stack developer, with a focus on mastering React.js."
+        />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return <img src={props.avatar} alt={props.name} className="avatar" />;
+}
+
+function Intro(props) {
+  return (
+    <div>
+      <h1>{props.IntroName}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  const skills = [
+    { name: "Ract Js", level: "Beginner", color: "#08DDFF" },
+    { name: "HTML", level: "Expert", color: "#ffcc00" },
+    { name: "CSS", level: "Advanced", color: "#0099ff" },
+    { name: "JavaScript", level: "Intermediate", color: "#ff3300" },
+    { name: "PHP", level: "Advanced", color: "#7377AD" },
+    { name: "MongoDB", level: "Intermediate", color: "#55AD47" },
+    { name: "MySql", level: "Advanced", color: "#08668E" },
+    { name: "Bootstrap", level: "Intermediate", color: "#7B09F8" },
+  ];
+  return (
+    <div className="skill-list">
+      {skills.map((skill, index) => (
+        <Skill
+          key={index}
+          name={skill.name}
+          level={skill.level}
+          color={skill.color}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Skill(props) {
+  const skillStyle = {
+    backgroundColor: props.color,
+    padding: "10px",
+    margin: "5px",
+    borderRadius: "5px",
+  };
+
+  return (
+    <div className="skill" style={skillStyle}>
+      <h5>
+        {props.name} {props.level}
+      </h5>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
+  rootElement,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
